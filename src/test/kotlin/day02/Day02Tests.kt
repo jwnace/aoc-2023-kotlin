fc51f0bd011057da5bf9dddb39e7e0302b9d337d
Joe Nace <jwnace@gmail.com>
2023-12-08 21:38:51 -0500

    added tests for day 2

---
+++ src/test/kotlin/day02/Day02Tests.kt
@@ -0,0 +1,52 @@
+package day02
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day02Tests {
+    @Test
+    fun `part1 example`() {
+        // arrange
+        val input = listOf(
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        )
+
+        // act
+        val actual = part1(input)
+
+        // assert
+        assertEquals(8, actual)
+    }
+
+    @Test
+    fun `part1 solution`() {
+        assertEquals(2348, solve1())
+    }
+
+    @Test
+    fun `part2 example`() {
+        // arrange
+        val input = listOf(
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        )
+
+        // act
+        val actual = part2(input)
+
+        // assert
+        assertEquals(2286, actual)
+    }
+
+    @Test
+    fun `part2 solution`() {
+        assertEquals(76008, solve2())
+    }
+}
